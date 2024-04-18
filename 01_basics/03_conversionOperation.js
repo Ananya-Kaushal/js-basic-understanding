f5d5c3f9f3403686f5b16cdbce1843b205198618
Ananya Kaushal <62956565+Ananya-Kaushal@users.noreply.github.com>
2024-04-18 16:16:40 +0000

    clearing Conversion confusions

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -42,3 +42,43 @@ let stringNumber=String(someNumber)
 console.log(typeof stringNumber);//string
 console.log(stringNumber);//"33"
 
+// *************** Operations ****************
+
+let value = 3
+let negValue =-value
+console.log(negValue);
+
+console.log(2+2);//4
+console.log(2-2);//0
+console.log(2*2);//4
+console.log(2/2);//1
+console.log(2%2);//0
+console.log(2**2);//4 (** => power of)
+
+let str1="Hello"
+let str2=" Ananya"
+
+let str3=str1+str2
+console.log(str3);//Hello Ananya
+
+console.log("1" + 2);//as string hence 12
+console.log(1 + "2");//as string hence 12
+console.log("1" + 2+ 2);//as string hence 122
+console.log(1 +2 +"2");//as number(1+2=3) then whole as sstring hence final o/p=>32
+
+console.log(+true);//1 (bad approach, clean code should be written)
+
+console.log(+ "");//0 (bad approach ,"" represents false hence 0 is the o/p)
+
+let num1, num2, num3
+num1 = num2 =num3 =2+2
+
+console.log([num1,num2,num3]);//[4, 4, 4]
+//(bad approach)
+
+let gameCounter=100;
+gameCounter++;
+console.log(gameCounter);//101
+++gameCounter;
+console.log(gameCounter);//102
+
