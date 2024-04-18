7402da974cb27aa8d06c7d736dd0c3fb585b84a9
Ananya Kaushal <62956565+Ananya-Kaushal@users.noreply.github.com>
2024-04-18 14:48:05 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";//treat all JS Code as Newer Version
+
+//alert(3+3) //we are using Node.js not Browser hence it's not working
+
+console.log(3+3)//code readability should be high
+console.log("Ananya")
+
+let name="Ananya"
+let age=24
+let isLoggedIn=true
+
+//number => -(2^53-1) to (2^53-1)
+//bigint => -(2^53-1) to (2^53-1)
+//string => use ing "" or '' but preferredto use ""
+//boolean => true or false
+//null => also standalone valueble(we can also assign null(empty) to a varial).Its object type
+//undefined => when value is not defined or assigned and its undefined type
+//symbol => for unique purpose or uniqueness
+
+//object => null is object type
+
+console.log(typeof "Ananya");//string
+console.log(typeof age);//number
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
