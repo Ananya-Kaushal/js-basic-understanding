b9b927ba118008d8fe474f32ba6ccc42eb4d7dc0
Ananya Kaushal <62956565+Ananya-Kaushal@users.noreply.github.com>
2024-04-19 07:23:16 +0000

    comparision completed

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -17,3 +17,16 @@ console.log(null <= 0);//true
 
 /* The reasson is that an equality(==) check and comparision(<,<=,>,>=) works differently
 Comparision converts null to a number treating it as 0 that's why (null>=0 and null<=0) gives o/p as true*/
+console.log("For Undefined");
+console.log(undefined > 0);//false
+console.log(undefined == 0);//false
+console.log(undefined != 0);//true
+console.log(undefined < 0);//false
+console.log(undefined >= 0);//false
+console.log(undefined <= 0);//false
+
+// strict check means === (it checks value as well as it's datatype)
+
+console.log("Strict check");
+console.log("2" == 2);//true
+console.log("2" === 2);//false as "2" is string type where as 2 is number type 
